5b523ddd38c6152ce4fe7d1e4e499c768630a672
EvanRussellTheCoder <52358324+EvanRussellTheCoder@users.noreply.github.com>
2026-07-05 22:59:58 -0400

    added valid anagram/palindrome

---
+++ validanagram.ts
@@ -0,0 +1,80 @@
+/*
+You’re given two strings, s and t.
+Return true if t is an anagram of s; otherwise, return false.
+An anagram contains exactly the same characters with exactly the same frequencies, 
+but the characters may appear in a different order
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Input: s = "rat", t = "car"
+Output: false
+
+Assume both strings contain only lowercase English letters.
+
+*/
+
+function validAnagram(s: string, t: string): boolean {
+
+    if (s.length !== t.length) {
+        return false
+    }
+
+    let freqs = new Map<string, number>();
+
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        const count = freqs.get(char) ?? 0;
+    
+        freqs.set(char, count + 1);
+    }
+
+    for (let i = 0; i < t.length; i++) {
+        const char = t[i];
+        const count = freqs.get(char);
+    
+        if (count === undefined || count === 0) {
+            return false;
+        }
+    
+        freqs.set(char, count - 1);
+    }
+
+    return true;
+
+}
+
+function validAnagramSort(s: string, t: string): boolean {
+    return s.split("").sort().join("") === t.split("").sort().join("")
+}
+
+//Do it myself
+function vAnagram(s: string, t: string): boolean {
+
+    if (s.length !== t.length) {
+        return false;
+    }
+
+    const grams = new Map<string, number>();
+
+    for (let i = 0; i < s.length; i++){
+        const char = s[i];
+        const count = grams.get(char) ?? 0;
+
+        grams.set(char, count + 1);
+    }
+
+    for (let i = 0; i < t.length; i++){
+        const char = t[i];
+        const count = grams.get(char);
+
+        if (count === undefined || count === 0) {
+            return false;
+        }
+
+        grams.set(char, count - 1);
+    }
+
+    return true;
+
+}
---
+++ validpalindrome.ts
@@ -0,0 +1,91 @@
+/*
+
+Given a string s, return true if it is a palindrome after:
+
+Converting uppercase letters to lowercase
+Removing all non-alphanumeric characters
+
+Otherwise, return false.
+
+Examples:
+
+Input: "A man, a plan, a canal: Panama"
+Output: true
+
+Input: "race a car"
+Output: false
+
+*/
+
+function validPalindrome(s: string): boolean {
+
+    const cleaned = s.replace(/[^a-z0-9]/gi, "").toUpperCase();
+
+    let left = 0;
+    let right = cleaned.length - 1;
+
+    while (left < right) {
+        if (cleaned[left] !== cleaned[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true
+}
+
+function validPalindrome2(s: string): boolean {
+
+    const cleaned = s.replace(/[^a-z0-9]/gi, "").toUpperCase();
+
+    let left = 0;
+    let right = cleaned.length - 1;
+
+    while (left < right) {
+        if (!isAlphanumeric(s[left])) {
+            left++;
+            continue;
+        }
+    
+        if (!isAlphanumeric(s[right])) {
+            right--;
+            continue;
+        }
+
+        if (s[left].toUpperCase() !== s[right].toUpperCase()) {
+            return false;
+        }
+        
+        left++;
+        right--;
+
+    }
+
+    return true;
+    
+}
+
+function isAlphanumeric(char: string): boolean {
+    return /[a-z0-9]/i.test(char);
+}
+
+//do it yourself
+function palindrome(s: string): boolean {
+
+    const cleaned = s.replace(/'a-z0-9'/gi, "").toUpperCase();
+
+    let left = 0;
+    let right = cleaned.length - 1;
+
+    while (left < right) {
+        if (cleaned[left] !== cleaned[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true;
+
+}
